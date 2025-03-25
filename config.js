c357dc015a49ecb26039a299eefd04fb90040f10
Beluuuuuuuuuuuu <153420745+Beluuuuuuuuuuuu@users.noreply.github.com>
2025-03-25 14:21:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["1044998751472844880", ""], 
+  mongodbUri : "mongodb+srv://beluga:dynoe@cluster0.wdvyszi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  embedColor: "#00FFFF",
+  activityName: "/help", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+      name: "Avinan",
+      password: "avinan",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  9323,
       secure: false
     }
   ]
